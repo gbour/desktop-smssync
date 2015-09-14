cc0464fbf0810935388ad6a81a005076eebaafbb
Guillaume Bour <guillaume@bour.cc>
2015-09-14 20:29:28 +0200

    [refactor] add black background in systray icon

--- src/systray.cpp
+++ src/systray.cpp
@@ -5,7 +5,7 @@
 #include "systray.h"
 
 Systray::Systray(QObject *parent) : QObject(parent),
-  m_systray(QIcon(":/resources/smssync-trayicon.png"))
+  m_systray(QIcon(":/resources/smssync-trayicon-black.png"))
 {
     QObject::connect(&m_systray, SIGNAL(activated(QSystemTrayIcon::ActivationReason)), this, SLOT(onActivated(QSystemTrayIcon::ActivationReason)));
     m_systray.show();
